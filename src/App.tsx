039eef75ba88bcaac917af455e2012ce1484a48e
Danil Alshanov <danil@Danil-MacBook-Pro.local>
2024-07-26 00:48:41 +0700

    title Power TU

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello <code>World</code> for Power TU!
+          Hello World for <code>Power TU</code>!
         </p>
         <a
           className="App-link"
