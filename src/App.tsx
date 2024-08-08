433e2f145710df2f1411a44fca48f79d7c5c7b53
Danil Alshanov <danil@Danil-MacBook-Pro.local>
2024-08-09 01:44:39 +1000

    TU-19 Simple button that make a request to backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Product } from "./Product";
 
 function App() {
+  const [data, setData] = React.useState<Product[]>([]);
+
+  const getData = async () => {
+    const fetchedData = await fetch("http://13.60.43.60:8000/products/");
+    const json = await fetchedData.json();
+    setData(json);
+  };
   return (
     <div className="App">
       <header className="App-header">
+        <button onClick={getData}>GET Data</button>
+        {data.map((p) => (
+          <p key={p.product_id}>
+            {p.product_id} - {p.name} - {p.price}
+          </p>
+        ))}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello World for <code>Power TU</code>!
