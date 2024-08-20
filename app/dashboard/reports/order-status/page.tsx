15dcf682e27aea9bd3c0d1066b5ab7e77833892c
Danil Alshanov <danil@Danil-MacBook-Pro.local>
2024-08-20 18:59:33 +1000

    total count table

--- app/dashboard/reports/order-status/page.tsx
+++ app/dashboard/reports/order-status/page.tsx
@@ -8,110 +8,69 @@ import { violetColor } from "@/components/primitives";
 import { apiUrl, fetcher, SWRparams } from "@/app/tools/fetcher";
 import { Countries } from "@/components/select-country";
 
-const option: echarts.EChartsOption = {
-  tooltip: {
-    trigger: "item",
-  },
-  legend: {
-    top: "5%",
-    left: "center",
-  },
-  series: [
-    {
-      name: "Access From",
-      type: "pie",
-      radius: ["40%", "70%"],
-      avoidLabelOverlap: false,
-      itemStyle: {
-        borderRadius: 7,
-        borderColor: "#fff",
-        borderWidth: 2,
-      },
-      label: {
-        show: false,
-        position: "center",
-      },
-      emphasis: {
-        label: {
-          show: true,
-          fontSize: 40,
-          fontWeight: "bold",
-        },
-      },
-      labelLine: {
-        show: false,
-      },
-      data: [
-        { value: 1048, name: "Search Engine" },
-        { value: 735, name: "Direct" },
-        { value: 580, name: "Email" },
-        { value: 484, name: "Union Ads" },
-        { value: 300, name: "Video Ads" },
-      ],
-    },
-  ],
-  backgroundColor: "rgba(0, 0, 0, 1)",
-};
+interface OrderStatusCount {
+  status: string;
+  count: number;
+}
 
 export default function OrderStatusPage() {
   const [country, setCountry] = useState<string>("");
-  const { data, error, isLoading } = useSWR<any[]>(
-    `${apiUrl}/annual-revenue-by-gender`,
+  const { data, error, isLoading } = useSWR<OrderStatusCount[]>(
+    `${apiUrl}/order-status-count`,
     fetcher,
     SWRparams,
   );
   const [option, setOption] = useState<echarts.EChartsOption>({});
 
   useEffect(() => {
-    let dataset: any = {};
-
-    setOption({
-      title: {
-        text: "Order Status Statistics",
-      },
-      tooltip: {
-        trigger: "item",
-      },
-      legend: {
-        top: "5%",
-        left: "center",
-      },
-      series: [
-        {
-          name: "Access From",
-          type: "pie",
-          radius: ["40%", "70%"],
-          avoidLabelOverlap: false,
-          itemStyle: {
-            borderRadius: 7,
-            borderColor: "#fff",
-            borderWidth: 2,
-          },
-          label: {
-            show: false,
-            position: "center",
-          },
-          emphasis: {
-            label: {
-              show: true,
-              fontSize: 40,
-              fontWeight: "bold",
-            },
-          },
-          labelLine: {
-            show: false,
-          },
-          data: [
-            { value: 1048, name: "Search Engine" },
-            { value: 735, name: "Direct" },
-            { value: 580, name: "Email" },
-            { value: 484, name: "Union Ads" },
-            { value: 300, name: "Video Ads" },
-          ],
+    if (data) {
+      setOption({
+        title: {
+          text: "Order Status Statistics",
         },
-      ],
-      backgroundColor: "rgba(0, 0, 0, 1)",
-    });
+        tooltip: {
+          trigger: "item",
+        },
+        legend: {
+          top: "5%",
+          left: "center",
+        },
+        series: [
+          {
+            name: "Access From",
+            type: "pie",
+            radius: ["40%", "70%"],
+            avoidLabelOverlap: false,
+            itemStyle: {
+              borderRadius: 7,
+              borderColor: "#fff",
+              borderWidth: 2,
+            },
+            label: {
+              show: false,
+              position: "center",
+            },
+            emphasis: {
+              label: {
+                show: true,
+                fontSize: 40,
+                fontWeight: "bold",
+              },
+            },
+            labelLine: {
+              show: false,
+            },
+            data: [
+              ...data.map((item) => ({
+                value: item.count,
+                name: item.status,
+              })),
+            ],
+          },
+        ],
+        backgroundColor: "rgba(0, 0, 0, 1)",
+      });
+    }
   }, [data?.length]);
 
   return (
@@ -122,10 +81,10 @@ export default function OrderStatusPage() {
       </h1>
       <p className="mt-3">
         This pie chart displays the overall statistics for order statuses,
-        specifically showing the proportions of orders that are “Completed,”
-        “Shipped,” and “Pending.” Each segment represents the percentage of
-        orders in each status, providing a quick and clear overview of how
-        orders are distributed across these three categories.
+        specifically showing the proportions of orders that are “Completed”,
+        “Shipped”, “Cancelled” and “Pending”. Each segment represents the
+        percentage of orders in each status, providing a quick and clear
+        overview of how orders are distributed across these three categories.
       </p>
 
       {<Countries setCountry={setCountry} />}
--- app/dashboard/reports/products-sold/page.tsx
+++ app/dashboard/reports/products-sold/page.tsx
@@ -1,47 +1,71 @@
 "use client";
-import React from "react";
+import { Spinner } from "@nextui-org/spinner";
 import {
   Table,
-  TableHeader,
-  TableColumn,
   TableBody,
-  TableRow,
   TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
 } from "@nextui-org/table";
+import { useState } from "react";
+import useSWR from "swr";
+
+import { apiUrl, fetcher, SWRparams } from "@/app/tools/fetcher";
+import { violetColor } from "@/components/primitives";
+import { Countries } from "@/components/select-country";
+
+interface TotalItemsSold {
+  id: number;
+  product_name: string;
+  total_sold: number;
+}
 
 export default function ProductsSoldPage() {
+  const [country, setCountry] = useState<string>("");
+  const { data, error, isLoading } = useSWR<TotalItemsSold[]>(
+    `${apiUrl}/total-items-sold`,
+    fetcher,
+    SWRparams,
+  );
+
   return (
     <div>
-      <h1 className="text-2xl">ProductsSoldPage</h1>
-      <Table className="mt-5">
-        <TableHeader>
-          <TableColumn>NAME</TableColumn>
-          <TableColumn>ROLE</TableColumn>
-          <TableColumn>STATUS</TableColumn>
-        </TableHeader>
-        <TableBody>
-          <TableRow key="1">
-            <TableCell>Tony Reichert</TableCell>
-            <TableCell>CEO</TableCell>
-            <TableCell>Active</TableCell>
-          </TableRow>
-          <TableRow key="2">
-            <TableCell>Zoey Lang</TableCell>
-            <TableCell>Technical Lead</TableCell>
-            <TableCell>Paused</TableCell>
-          </TableRow>
-          <TableRow key="3">
-            <TableCell>Jane Fisher</TableCell>
-            <TableCell>Senior Developer</TableCell>
-            <TableCell>Active</TableCell>
-          </TableRow>
-          <TableRow key="4">
-            <TableCell>William Howard</TableCell>
-            <TableCell>Community Manager</TableCell>
-            <TableCell>Vacation</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
+      <h2 className="text-2xl">
+        The table shows the <span className={violetColor}>total count</span> of
+        items sold
+      </h2>
+      <p className="mt-3">
+        This table presents the total count of each item sold over the course of
+        a year. It provides a detailed breakdown of sales figures for individual
+        products, offering insights into the popularity and demand for each
+        item. This data can be used to analyze sales performance, identify
+        top-selling products, and inform inventory and purchasing decisions.
+      </p>
+
+      {<Countries setCountry={setCountry} />}
+
+      {(isLoading && (
+        <div className="w-full justify-center flex items-center">
+          <Spinner size="lg" />
+        </div>
+      )) ||
+        (data && (
+          <Table className="mt-5">
+            <TableHeader>
+              <TableColumn>Product Name</TableColumn>
+              <TableColumn>Total Count Sold</TableColumn>
+            </TableHeader>
+            <TableBody>
+              {data.map((item) => (
+                <TableRow key={item.id}>
+                  <TableCell>{item.product_name}</TableCell>
+                  <TableCell>{item.total_sold}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        ))}
     </div>
   );
 }
